6db074b0442ddb97596dddd5a0103457cbbc4179
Mohammed El-kabli <melkabli05@gmail.com>
2021-02-11 09:06:43 +0100

    pushing

---
+++ memorial-explain.bat
@@ -0,0 +1,18 @@
+<h1>Une grille avec CSS Grid !</h1>
+
+<div class="grille">
+    <div class="grid-item"></div>
+    <div class="grid-item"></div>
+    <div class="grid-item"></div>
+    <div class="grid-item"></div>
+
+    <div class="grid-item"></div>
+    <div class="grid-item"></div>
+    <div class="grid-item"></div>
+    <div class="grid-item"></div>
+
+    <div class="grid-item"></div>
+    <div class="grid-item"></div>
+    <div class="grid-item"></div>
+    <div class="grid-item"></div>
+</div>
